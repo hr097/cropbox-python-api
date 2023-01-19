77032a345dc4286ee72f76fff3f2ab0a64b6ca08
hr097 <harshilramani9777@gmail.com>
2023-01-19 16:48:36 +0530

    Final Update of API

--- index.py
+++ index.py
@@ -50,10 +50,9 @@ def delete_file():
             return Response(json.dumps({"status": True,"code": 404,"message": "File not found!"}), mimetype="application/json")     
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
 
-        req  = json.loads(request.data)
+@app.route('/upload-for-flipkart', methods=['POST'])
+def uploadFlipkart():
 
         try:
 
@@ -68,12 +67,8 @@ def upload():
                 output = PdfWriter()
 
                 numPages = len(input1.pages)
-                if (req['type']=="Meesho"): #Meesho
-                      x, y, w, h = (5,5,582,590)
-                elif (req['type']=="Flipkart"): #Flipkart
-                     x, y, w, h = (170, 23,255, 350)
-                else:
-                    return Response(json.dumps({'status': True,'code': 417,'message': 'Type not defined'}), mimetype='application/json') # PDF cropped and created
+
+                x, y, w, h = (170, 23,255, 350)
 
                 page_x= input1.pages[0].cropbox.left
                 page_y= input1.pages[0].cropbox.top 
@@ -98,7 +93,51 @@ def upload():
         except Exception as e:
             app.logger.info("error occurred")
             return Response(json.dumps({'status': True,'code': 503,'message': 'Something Went Wring! Please Try Again Later'}), mimetype='application/json') #something went wrong while cropping and making new PDF
-    
+
+
+@app.route('/upload-for-meesho', methods=['POST'])
+def uploadMeesho():
+
+        try:
+
+            pdf_file = request.files['filename']
+            pdf_name = pdf_file.filename
+            save_path = os.path.join(app.config.get('upload_folder'),pdf_name)
+            pdf_file.save(save_path)
+
+            with open(save_path, "rb") as in_f:
+
+                input1 = PdfReader(in_f)
+                output = PdfWriter()
+
+                numPages = len(input1.pages)
+
+                x, y, w, h = (5,5,582,590)
+
+                page_x= input1.pages[0].cropbox.left
+                page_y= input1.pages[0].cropbox.top 
+                upperLeft = [page_x.as_numeric(), page_y.as_numeric()] 
+                new_upperLeft  = (upperLeft[0] + x, upperLeft[1] - y)
+                new_lowerRight = (new_upperLeft[0] + w, new_upperLeft[1] - h)
+
+                for i in range(numPages):
+                    page = input1.pages[i]
+                    page.cropbox.lower_left  = new_upperLeft
+                    page.cropbox.lower_right = new_lowerRight
+                    output.add_page(page)
+
+                with open(os.path.join(app.config.get('upload_folder'), "output_"+pdf_name), "wb") as out_f:
+                    output.write(out_f)
+
+            if os.path.exists(os.path.join(app.config.get('upload_folder'), pdf_name)):
+                        os.remove(os.path.join(app.config.get('upload_folder'), pdf_name))
+
+            return Response(json.dumps({'status': True,'code': 200,'message': 'Processed','filename':"output_"+pdf_name}), mimetype='application/json') # PDF cropped and created
+        
+        except Exception as e:
+            app.logger.info("error occurred")
+            return Response(json.dumps({'status': True,'code': 503,'message': 'Something Went Wring! Please Try Again Later'}), mimetype='application/json') #something went wrong while cropping and making new PDF
+
 
 # main method
 if __name__ == "__main__":
